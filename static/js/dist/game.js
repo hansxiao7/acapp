547ab367d1b8fa3e4cbbb68e46ac256f7cfb948f
hansxiao7 <hansxiao7@gmail.com>
2021-11-15 14:58:59 +0800

    bug fix for canvas pos

--- static/js/dist/game.js
+++ static/js/dist/game.js
@@ -217,11 +217,12 @@ class Player extends AcGameObject{
         this.playground.gamemap.$canvas.on("contextmenu", function(){return false;});
 
         this.playground.gamemap.$canvas.mousedown(function(e) {
+            const rect = outer.ctx.canvas.getBoundingClientRect();
             if (e.which === 3){
-                outer.move_to(e.clientX, e.clientY);
+                outer.move_to(e.clientX - rect.left, e.clientY - rect.top);
             } else if (e.which === 1){
                 if (outer.cur_skill === 'fireball'){
-                    outer.shoot_fireball(e.clientX, e.clientY);
+                    outer.shoot_fireball(e.clientX - rect.left, e.clientY - rect.top);
                 }
 
                 outer.cur_skill = null;
