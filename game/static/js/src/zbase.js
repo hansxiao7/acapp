8fbcecb3a02a5ee6b369e9a7d94f6466586f4cc9
hansxiao7 <hansxiao7@gmail.com>
2021-11-24 16:42:36 +0800

    add login/register/logout

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -2,9 +2,11 @@ export class AcGame {
     constructor(id) {
         this.id = id;
         this.$ac_game = $('#' + id);
+        console.log('abc');
+        this.settings = new Settings(this);
         this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
-        
+
         this.start();
     }
 
